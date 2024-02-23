ef736022aa192087dbf62892b1a9037ccde36ca3
NhatNam7777 <namnguyentmrm@gmail.com>
2024-02-23 14:51:42 +0700

    increase 1

--- lib/counter_model.dart
+++ lib/counter_model.dart
@@ -4,7 +4,7 @@ class CounterModel extends ChangeNotifier {
   int _count = 0;
   int get count => _count;
   void increment() {
-    _count = _count + 2;
+    _count = _count++;
     notifyListeners();
   }
 }
